0240e63168ec14d82c565dacd449cae61334f787
KADEEJA-SHAMEER <kadeejashameer110@gmail.com>
2025-06-03 21:15:20 +0530

    updated

---
+++ pattern7.py
@@ -0,0 +1,10 @@
+n=int(input("enter the number of rows : "))
+i=1
+if n>=1:
+    while i<=n:
+        sqrt_n=i**0.5
+        if (sqrt_n*sqrt_n)==i:
+            print("* "*(i*2))
+        else:
+            print("* "*i)
+        i=i+1
